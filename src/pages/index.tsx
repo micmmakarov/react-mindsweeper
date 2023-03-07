534047fbab8467ed64344ebf95f7416f64c98111
Miša Mаkaröv <info1117@gmail.com>
2023-03-06 20:28:32 -0500

    Fixes types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,25 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Game from 'mindsweeper'
+import Mindsweeper from 'mindsweeper'
+import { type Options } from 'mindsweeper/dist/types/game'
 
-const defaultSize = 30;
-const defaultMines = 100;
+const defaultSize = 20;
+const defaultMines = 80;
+const defaultOptions : Options = {size:{width: defaultSize, height: defaultSize}, mines: defaultMines};
+const defaultGame = new Mindsweeper.Game(defaultOptions);
+interface Param {
+  width?: number
+  height?: number
+  mines?: number
+}
 
+const defaultParam : Param = {};
 
 export default function Home() {
-  const [game, setGame] = useState(new Game({size:{width: defaultSize, height: defaultSize}, mines: defaultMines}));
-  const [params, setParams] = useState({});
+  
+  const [game, setGame] = useState(defaultGame);
+  const [params, setParams] = useState(defaultParam);
   
   const updateParams = (e: any) => {
     const name: string = e.target.getAttribute('name');
@@ -22,7 +32,7 @@ export default function Home() {
 
   const generateGame = () => {
     setGame(
-      new Game({size:{width: params.width || defaultSize, height: params.height || defaultSize}, mines: params.mines || defaultMines})
+      new Mindsweeper.Game({size:{width: params.width || defaultSize, height: params.height || defaultSize}, mines: params.mines || defaultMines})
     )
   }
 
